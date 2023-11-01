8fb914c7c8a01e2ed5fde878d0c55e3a7f37ac2a
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2023-11-01 16:25:52 -0300

    remove contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', {'name': 'Bruno Pereira'
                                                  'de Souza'})
-
-
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
