8625a006a234989f66ff2accea76c285c843289d
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-01-28 20:19:59 -0300

    filtrando as receitas por categoria

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+admin.site.register(Category)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
