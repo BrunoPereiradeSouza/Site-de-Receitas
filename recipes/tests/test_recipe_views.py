17f30f1cc2703ce92ba262ab92d9d21aebf7a51f
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-04 07:47:12 -0300

    testando o status_code retornado pelas view_functions

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -8,10 +8,33 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipe_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes found here 😢</h1>',
+            response.content.decode('utf-8'))
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
 
+    def test_recipe_category_view_function_return_404_if_no_recipes_found(self):  # noqa: E501
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_recipes_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_recipes_view_function_return_404_if_no_recipe_found(self):  # noqa: E501
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, 404)
