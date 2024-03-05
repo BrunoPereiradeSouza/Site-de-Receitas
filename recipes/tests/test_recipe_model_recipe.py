236f3905732af6a0139a6eb883902ab774b1be6f
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-05 17:25:17 -0300

    testando a string representation da Recipe

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -52,3 +52,11 @@ class RecipeModelTest(RecipeTestBase):
             recipe.is_published,
             msg='Recipe is_published is not False',
             )
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+
+        self.assertEqual(str(self.recipe), needed)
