df9b22188360e8a52f1e3aee79f0a16543812779
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-11 17:51:09 -0300

    adicionando atributos nos campos

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,45 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholer(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholer(self.fields['username'], 'Your username')
+        add_placeholer(self.fields['email'], 'Your e-mail')
+        add_placeholer(self.fields['first_name'], 'Ex.: John')
+        add_placeholer(self.fields['last_name'], 'Ex.: Doe')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        error_messages={
+            'required': 'Password mut not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter, one lowercase'
+            ' letter and one number. The length should be at least 8 characters'  # Noqa: E501
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        }),
+    )
+
     class Meta:
         model = User
         fields = [
