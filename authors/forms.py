98881966c2543d0654c317e3e16319494e00bce5
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-11 15:26:59 -0300

    configurando widgets, erros e atributos do forms

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,34 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
               ]
+
+        # serve para definir o nome que será exibido no label de cada campo
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        # Definir mensagens de ajuda nos campos
+        help_texts = {
+            'email': 'the e-mail must be valid.',
+        }
+
+        # Definir mensagens de erro nos campos
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here',
+                'class': 'form-control',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
