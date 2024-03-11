a8359f6551c30a0366ac71fbb111d2f2c4804ae2
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-11 13:59:46 -0300

    usando session para salvar dados entre requests

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import Http404
 
 
 def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
-
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     context = {'form': form}
     return render(request, 'authors/pages/register_view.html', context)
+
+
+def register_create(request):
+    if request.method != 'POST':
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
+
