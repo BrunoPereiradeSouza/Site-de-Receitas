2549c5e90b0c95fd2bf93e09c9d3557c8915b55e
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-11 11:25:56 -0300

    adicionando os campos do form manualmente

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,10 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+
     context = {'form': form}
     return render(request, 'authors/pages/register_view.html', context)
