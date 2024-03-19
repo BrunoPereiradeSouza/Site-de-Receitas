70948da2ed0910347bfe641ae940efb980ed2a25
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-19 12:09:47 -0300

    Criando as Views, Urls e o Template para login do usuário

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -74,7 +74,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
     ])
     def test_fields_cannot_be_empty(self, field, msg):
         self.form_data[field] = ''
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
 
         self.assertIn(msg, response.context['form'].errors.get(field))
@@ -84,7 +84,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.form_data['username'] = 'eu'
         error_msg = ('Username must have at least 4 characters')
 
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
 
         self.assertIn(error_msg, response.content.decode('utf-8'))
@@ -94,7 +94,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.form_data['username'] = 'eu' * 76
         error_msg = ('Username cannot have more than 150 characters')
 
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
 
         self.assertIn(error_msg, response.content.decode('utf-8'))
@@ -106,7 +106,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
                      'one lowercase letter and one number. The length should'
                      ' be at least 8 characters')
 
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.post(url, data=self.form_data, follow=True)
 
         self.assertIn(error_mgs, response.context['form'].errors.get('password'))  # Noqa: E501
@@ -115,7 +115,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.form_data['password'] = 'Acd321212'
         error_msg = 'password and password2 must be equal'
 
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response1 = self.client.post(url, data=self.form_data, follow=True)
 
         self.assertIn(error_msg, response1.context['form'].errors.get('password'))  # Noqa: E501
@@ -127,13 +127,13 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertNotIn(error_msg, response2.content.decode('utf-8'))
 
     def test_send_get_request_to_create_view_returns_404(self):
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 404)
 
     def test_email_field_returns_error_if_email_already_exists(self):
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
 
         self.client.post(url, data=self.form_data, follow=True)
         response = self.client.post(url, data=self.form_data, follow=True)
@@ -144,7 +144,7 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         self.assertIn(msg, response.content.decode('utf-8'))
 
     def test_author_created_can_login(self):
-        url = reverse('authors:create')
+        url = reverse('authors:register_create')
 
         self.form_data.update({
             'username': 'testuser',
--- authors/views.py
+++ authors/views.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
-    context = {'form': form, 'form_action': reverse('authors:create')}
+    context = {'form': form, 'form_action': reverse('authors:register_create')}
     return render(request, 'authors/pages/register_view.html', context)
 
 
@@ -29,3 +29,11 @@ def register_create(request):
         del (request.session['register_form_data'])
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    return render(request, 'authors/pages/login.html')
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
