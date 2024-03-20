b42a70bd9b79317f1443c801d458c9039c468b3c
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-20 08:12:42 -0300

    Authenticando o usuário no sistema

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,7 @@ from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 
 def register_view(request):
@@ -40,4 +41,22 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', '')
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'your are logged in')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'invalid username or password')
+
+    return redirect('authors:login')
