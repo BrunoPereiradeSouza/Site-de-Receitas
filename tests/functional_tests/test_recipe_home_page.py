cf60fd6bff5d045b33204e5732778d08d274576c
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-03-29 19:00:05 -0300

    FunctionalTests: Movendo as repetições para SetUp e TearDown

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -4,14 +4,21 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        self.browser.get(self.live_server_url)
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         sleep(seconds)
 
-    def test_the_test(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
-        self.sleep(2)
-        body = browser.find_element(By.TAG_NAME, 'body')
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 😢', body.text)
-        browser.quit()
--- utils/browser.py
+++ utils/browser.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import os
 
 
 ROOT_PATH = Path(__file__).parent.parent
@@ -14,6 +15,9 @@ def make_chrome_browser(*options):
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
 
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
