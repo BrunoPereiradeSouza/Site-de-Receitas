c6a9256c2642971f65dcf5cdb8600ab20494175e
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-04-06 20:04:34 -0300

    Criando o método post e reorganizando a ClassBasedView

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -4,36 +4,56 @@ from authors.forms import AuthorRecipeForm
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import Http404
 
 
 class DashboardRecipe(View):
-    def get(self, request, id):
-        recipe = Recipe.objects.get(
-            author=request.user, is_published=False, id=id
+    def get_recipe(self, id):
+        recipe = None
+
+        if id:
+            recipe = Recipe.objects.get(
+                author=self.request.user, is_published=False, id=id
+            )
+
+        if not recipe:
+            raise Http404
+
+        return recipe
+
+    def render_recipe(self, form):
+        return render(
+            self.request, 'authors/pages/dashboard_recipe.html',
+            context={'form': form}
         )
 
-        if request.method == 'POST':
-            form = AuthorRecipeForm(
-                request.POST or None,
-                request.FILES or None,
-                instance=recipe
+    def get(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+
+        return self.render_recipe(form)
+
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
+
+        form = AuthorRecipeForm(
+            request.POST or None,
+            request.FILES or None,
+            instance=recipe
             )
-            if form.is_valid():
-                recipe = form.save(commit=False)
 
-                recipe.author = request.user
-                recipe.preparation_steps_is_html = False
-                recipe.is_published = False
-                recipe.save()
+        if form.is_valid():
+            recipe = form.save(commit=False)
 
-                messages.success(request, 'Sua receita foi salva com sucesso!')
-                return redirect(reverse(
-                    'authors:dashboard_recipe_edit', args=(id,)
-                    )
+            recipe.author = request.user
+            recipe.preparation_steps_is_html = False
+            recipe.is_published = False
+            recipe.save()
+
+            messages.success(request, 'Sua receita foi salva com sucesso!')
+            return redirect(reverse(
+                'authors:dashboard_recipe_edit', args=(id,)
                 )
-        else:
-            form = AuthorRecipeForm(instance=recipe)
+            )
 
-        return render(request, 'authors/pages/dashboard_recipe.html', context={
-            'form': form
-        })
+        return self.render_recipe(form)
