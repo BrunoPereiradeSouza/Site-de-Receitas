538d55d5ca547c02446fe7ff7a2ed491b9a89789
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-04-15 17:51:51 -0300

    Adicionando JsonResponse para DetailView

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
         views.RecipeListViewHomeApi.as_view(),
         name='recipes_api_v1'
         ),
+    path(
+        'recipes/api/v1/<int:pk>/',
+        views.RecipeDetailViewApi.as_view(),
+        name='recipes_api_v1_detail'
+    ),
 ]
--- recipes/views/recipe_detail_view.py
+++ recipes/views/recipe_detail_view.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView
 from recipes.models import Recipe
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 
 class RecipeDetailView(DetailView):
@@ -23,3 +25,26 @@ class RecipeDetailView(DetailView):
         )
 
         return context
+
+
+class RecipeDetailViewApi(RecipeDetailView):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        recipe_dict['created_at'] = recipe.created_at
+        recipe_dict['updated_at'] = recipe.updated_at
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False
+        )
