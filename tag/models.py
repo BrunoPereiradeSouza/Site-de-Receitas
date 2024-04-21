968018906fd2817cb683958501476052f768cb71
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-04-21 14:08:20 -0300

    Criando o Model Tag com relação genérica

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils.text import slugify
+import string
+from random import SystemRandom
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    # Aqui começa os campos para a relação genérica
+
+    # Representa o Model que queremos encaixar aqui
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # Representa o id da linha do Model descrito acima
+    object_id = models.CharField(max_length=255)
+    # Um campo que representa a relação genérica que conhece os
+    # campos acima (content_type e object_id)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            rand_letters = ''.join(
+                SystemRandom().choices(
+                    string.ascii_letters + string.digits,
+                    k=5,
+                )
+            )
+            self.slug = slugify(f'{self.name}-{rand_letters}')
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
