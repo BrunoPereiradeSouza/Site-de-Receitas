7d837c3b1b61e5ef8930f79ad76418cbca1281b9
BrunoPereiradeSouza <brunosouzaff10@gmail.com>
2024-05-15 16:37:17 -0300

    Renomeando e juntando campos no serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,3 +5,8 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField()
+
+    def get_preparation(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
